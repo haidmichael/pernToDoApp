3e2564aed49a7514ed4176b7f226a1d67de96350
Mike Haid <haidmichaelj@gmail.com>
2025-10-18 17:27:43 -0500

    added dotenv, implemented postgresql db, started to work on db connection.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import { pool } from './db.js'
 
 const app = express() 
 const PORT = 5000
@@ -7,6 +8,25 @@ const PORT = 5000
 // Middleware
 app.use(cors())
 app.use(express.json()) // gets access to req.body
+
+// Routes 
+
+// create a todo
+app.post('/todos', async (req, res) => {
+    try {
+        console.log(req.body)
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo 
 
 app.listen(PORT, () => {
     console.log(`Server is working down here on Port ${PORT}`)
