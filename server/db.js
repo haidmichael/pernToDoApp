9a350d9d9e5d6963f5b8050835654d2b14fc693c
Mike Haid <haidmichaelj@gmail.com>
2025-10-23 09:01:34 -0500

    updated db values, changed server port. Added post route, implemented test route.

--- server/db.js
+++ server/db.js
@@ -2,12 +2,20 @@
 import dotenv from 'dotenv' 
 import pkq from 'pg'
 const { Pool } = pkq 
+dotenv.config()
 
 export const pool = new Pool({
-
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  if(err) {
+    console.error('Error connecting to the database', err.stack)
+  } else {
+    console.log('Database connected successfully')
+  }
 })
--- server/index.js
+++ server/index.js
@@ -1,23 +1,38 @@
 import express from 'express'
+const app = express() 
 import cors from 'cors'
 import { pool } from './db.js'
+const PORT = 3000
 
-const app = express() 
-const PORT = 5000
 
 // Middleware
-app.use(cors())
 app.use(express.json()) // gets access to req.body
+app.use(cors())
 
 // Routes 
 
 // create a todo
 app.post('/todos', async (req, res) => {
     try {
-        console.log(req.body)
+        const { description } = req.body 
+        // console.log(req.body)
+        const newTodo = await pool.query(
+            'INSERT INTO todo (description) VALUES($1)', 
+            [description]
+        )
+        res.json(newTodo)
     } catch (err) {
         console.error(err.message)
     }
+})
+
+
+// ########## Testing Connection ##########
+app.get('/health', async(req, res) => {
+    return res.status(200).json({
+        message: 'Okay', 
+        status: '200'
+    })
 })
 
 // get all todos
